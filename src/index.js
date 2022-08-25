c81c68dbc2d377f9019aab8bf9f0f32a405132d8
Roni 88 <ronix88x@hotmail.com>
2022-08-25 00:04:52 -0300

    mudando a lógica

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ var fundo = document.getElementById ('fundo')
 
 msg.innerHTML = `<h3>Agora são ${hora} horas.</h3>`
 
-if (hora >= 0 && hora <= 12) {
+if (hora >= 0 && hora < 12) {
     msg.innerHTML += `Bom dia!`
     img.src = 'src/imagens/amanhecer.jpg'
     fundo.style.backgroundColor = 'rgb(147, 206, 243)'
     fundo.style.color = 'rgb(22, 22, 192)'
-} else if (hora >=12 && hora < 18) {
+} else if (hora >= 12 && hora < 18) {
     msg.innerHTML += `Boa tarde!`
     img.src = 'src/imagens/entardecer.jpg'
     fundo.style.backgroundColor = 'rgb(234, 125, 125)'
